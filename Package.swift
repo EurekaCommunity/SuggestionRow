018a502102d7720450b10d27efaf2f0c7724cf02
August Heegaard <august@heegaard.org>
2020-02-26 18:28:29 +0100

    Add support for Swift Package Manager (#49)

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "SuggestionRow",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "SuggestionRow", targets: ["SuggestionRow"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/xmartlabs/Eureka.git", from: "5.2.0"),
+    ],
+    targets: [
+        .target(
+            name: "SuggestionRow",
+            dependencies: ["Eureka"],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "SuggestionRowTests",
+            dependencies: ["SuggestionRow"],
+            path: "SuggestionRowTests"
+        )
+    ]
+)
--- Sources/SuggestionCollectionViewCell.swift
+++ Sources/SuggestionCollectionViewCell.swift
@@ -5,7 +5,7 @@
 //
 //
 
-import Foundation
+import UIKit
 import Eureka
 
 
